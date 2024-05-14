636ca81adf14730d8710561947c59f31ad5db756
Lucas Araújo <113523243+JLFullStack@users.noreply.github.com>
2024-05-13 21:24:34 -0300

    java doc
    estudos sobre o java doc

---
+++ Java/curso-001-dio-formacao-java-developer/001-fundamentos-da-plataforma-java/projeto-002/sintaxe-java/src/java_doc/Calculadora.java
@@ -0,0 +1,25 @@
+package java_doc;
+
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recorsos oferecidos pelo autor
+ * 
+ * @author Lucas Araújo
+ * @verion 1.0
+ * @since 13/05/2024
+ */
+
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado desde método é a soma dos dois números.
+     */
+     public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+     }
+}
--- Java/curso-001-dio-formacao-java-developer/001-fundamentos-da-plataforma-java/projeto-002/sintaxe-java/src/metodos/Usuario.java
+++ Java/curso-001-dio-formacao-java-developer/001-fundamentos-da-plataforma-java/projeto-002/sintaxe-java/src/metodos/Usuario.java
@@ -4,21 +4,27 @@ public class Usuario {
     public static void main(String[] args) {
         SmartTv smartTv = new SmartTv();
 
-        // System.out.println("TV Ligada? " + smartTv.ligada);
-        // System.out.println("Canal Atual: " + smartTv.canal);
-        // System.out.println("Volume: " + smartTv.volume);
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume: " + smartTv.volume);
 
-        //smartTv.ligar();
-        //System.out.println("TV Ligada? " + smartTv.ligada);
+        smartTv.ligar();
+        System.out.println("TV Ligada? " + smartTv.ligada);
 
-        //smartTv.desligar();
-        //System.out.println("TV Ligada? " + smartTv.ligada);
+        smartTv.desligar();
+        System.out.println("TV Ligada? " + smartTv.ligada);
 
-        //smartTv.aumentarVolume();
-        //System.out.println("volume: " + smartTv.volume);
+        smartTv.aumentarVolume();
+        System.out.println("volume: " + smartTv.volume);
 
-        //smartTv.diminuirVolume();
-        //System.out.println("volume: " + smartTv.volume);
+        smartTv.diminuirVolume();
+        System.out.println("volume: " + smartTv.volume);
+
+        smartTv.aumentarCanal();
+        System.out.println("volume: " + smartTv.canal);
+
+        smartTv.diminuirCanal();
+        System.out.println("volume: " + smartTv.canal);
 
         smartTv.mudarCanal(15);
         System.out.println("Canal Atual: " + smartTv.canal);
