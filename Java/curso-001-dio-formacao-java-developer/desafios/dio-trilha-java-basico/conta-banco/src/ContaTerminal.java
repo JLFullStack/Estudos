efbc0b2a5d361593d3d06e078ed32364e70a8ce7
Lucas Araújo <113523243+JLFullStack@users.noreply.github.com>
2024-05-17 18:01:55 -0300

    Desafio Conta Banco
    criação do desafia de conta bancária em Java

--- Java/curso-001-dio-formacao-java-developer/desafios/dio-trilha-java-basico/conta-banco/src/ContaTerminal.java
+++ Java/curso-001-dio-formacao-java-developer/desafios/dio-trilha-java-basico/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //criando um objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Conta !");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite sua Agência !");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite seu nome !");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite seu saldo !");
+        double saldoConta = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo de " + saldoConta + " já está disponível para saque.");
     }
 }
